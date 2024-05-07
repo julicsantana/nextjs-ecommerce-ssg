dce80386f674b68fc2beb7dde3b71233c83d0ebb
Juliana Santana <juliana.c.s9898@gmail.com>
2024-05-07 16:32:51 -0300

    adds config to render images staticaly

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,36 @@
 import styles from "./page.module.css";
 import { Categorias } from "./components/Categorias";
 import { Produtos } from "./components/Produtos";
-import { getCategorias } from "../lib/api";
+
+async function fetchProdutosApi() {
+  const res = await fetch(
+    "https://api.npoint.io/559ae4cec77502bd2e87/produtos"
+  );
+
+  if (!res.ok) {
+    throw new Error("Não foi possível obter os dados.");
+  }
+
+  const produtos = await res.json();
+  return produtos;
+}
+
+async function fetchCategoriasApi() {
+  const res = await fetch(
+    "https://api.npoint.io/3b8156d7d3e6d64a1a78/categorias"
+  );
+
+  if (!res.ok) {
+    throw new Error("Não foi possível obter os dados.");
+  }
+
+  const categorias = await res.json();
+  return categorias;
+}
 
 export default async function Home() {
   const produtos = await fetchProdutosApi();
-  const categorias = getCategorias();
-
-  async function fetchProdutosApi() {
-    const res = await fetch("http://localhost:3000/api/produtos");
-
-    if (!res.ok) {
-      throw new Error("Não foipossível obter os dados.");
-    }
-
-    const produtos = await res.json();
-    return produtos;
-  }
+  const categorias = await fetchCategoriasApi();
 
   return (
     <>
--- src/app/produto/[slug]/page.jsx
+++ src/app/produto/[slug]/page.jsx
@@ -1,13 +1,39 @@
 import styles from "./page.module.css";
 import Produto from "@/app/components/Produto";
-import { getProdutoPorSlug } from "../../../lib/api";
+
+async function fetchProdutoBySlug(slug) {
+  const res = await fetch(
+    `https://api.npoint.io/559ae4cec77502bd2e87/produtos`
+  );
+
+  if (!res.ok) {
+    throw new Error("Não foi possível obter os dados.");
+  }
+
+  const produtos = await res.json();
+  const produto = produtos.find((prod) => prod.id.toString() === slug);
+
+  return produto;
+}
 
 export default async function ProdutoPage({ params }) {
-  const produto = getProdutoPorSlug(params.slug);
+  const produto = await fetchProdutoBySlug(params.slug);
 
   return (
     <main className={styles.main}>
       <Produto produto={produto} />
     </main>
   );
+}
+
+export async function generateStaticParams() {
+  const res = await fetch(
+    "https://api.npoint.io/559ae4cec77502bd2e87/produtos"
+  );
+  const produtos = await res.json();
+  const result = produtos.map((produto) => ({
+    slug: produto.id.toString(),
+  }));
+
+  return result;
 }
