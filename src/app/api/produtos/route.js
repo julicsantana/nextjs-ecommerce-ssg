26980ab0bd53810719175be0162e0efcbdaa3bb0
Juliana Santana <juliana.c.s9898@gmail.com>
2024-05-07 15:00:16 -0300

    first commit

---
+++ src/app/api/produtos/route.js
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+import { getTodosProdutos } from "@/lib/api";
+
+export async function GET() {
+  const produtos = getTodosProdutos();
+  return NextResponse.json(produtos);
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,22 @@
 import styles from "./page.module.css";
 import { Categorias } from "./components/Categorias";
 import { Produtos } from "./components/Produtos";
-import { getCategorias, getTodosProdutos } from "../lib/api";
+import { getCategorias } from "../lib/api";
 
 export default async function Home() {
-  const produtos = getTodosProdutos();
+  const produtos = await fetchProdutosApi();
   const categorias = getCategorias();
+
+  async function fetchProdutosApi() {
+    const res = await fetch("http://localhost:3000/api/produtos");
+
+    if (!res.ok) {
+      throw new Error("Não foipossível obter os dados.");
+    }
+
+    const produtos = await res.json();
+    return produtos;
+  }
 
   return (
     <>
